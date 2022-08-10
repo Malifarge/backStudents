904b0dc19aba8f05d1fbdfc520859b3dffe558cd
Thomas <malifarge.thomas9@gmail.com>
2022-08-10 11:49:26 +0200

    clean students

--- routes/students.js
+++ routes/students.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const students= require('./students.json')
+const students= []
 
 app.get('/students',(req,res)=>{
     res.json(students)
