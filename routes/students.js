7595721dd204f0e7547ba1662cfb818d5af28fac
Thomas <malifarge.thomas9@gmail.com>
2022-08-10 09:56:51 +0200

    not double

--- routes/students.js
+++ routes/students.js
@@ -10,10 +10,18 @@ app.post('/students', (req, res) => {
     const student = {
       name: req.body.name,
     }
-  
-    students.push(student)
-  
-    res.json(student)
+    const test = students.find(studentbase=>{
+     return studentbase.name === student.name
+    } )
+
+    console.log(test);
+
+    if (!test){
+      students.push(student)
+      res.json(student)
+    }else{
+      res.status(409).send("student already exists")
+    }
   })
 
 module.exports = app
